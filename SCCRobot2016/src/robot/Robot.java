dfd71a0dc1e10d3c865e1b8ed6763c5626fd1562
flepeint <flepeint@gmail.com>
2016-04-30 17:16:26 -0700

    Updated Robot class

--- SCCRobot2016/src/robot/Robot.java
+++ SCCRobot2016/src/robot/Robot.java
@@ -5,4 +5,14 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
 	public static Drive drive = new Drive();
+
+	/**
+	 * This method is run when the robot is first started up and should be used
+	 * for any initialization code.
+	 */
+	public void robotInit() {
+		RobotMap.init();
+		DriverStation.buttonInit();
+	}
+
 }
